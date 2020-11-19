d3c9f119e94a5376aa62e6fc8d8715e274b0d649
zhenhua.zhang@alo7.com <zhenhua.zhang@alo7.com>
2020-11-19 23:12:34 +0800

    feat: 添加spring-data-jpa原理与实战源码22章 spring.data.jpa.open-in-view 案例

--- 2.3/jpa-demo/src/main/java/com/example/jpa/demo/web/UserInfoController.java
+++ 2.3/jpa-demo/src/main/java/com/example/jpa/demo/web/UserInfoController.java
@@ -17,7 +17,15 @@ public class UserInfoController {
 	}
 
 	@PostMapping("/user/info")
-	public UserInfo saveUserInfo(@RequestBody UserInfo userInfo) {
+	public UserInfo saveUserInfo(@RequestBody UserInfo userInfo) throws InterruptedException {
+		UserInfo u2 = userInfoRepository.findById(1L).orElse(null);
+		if (u2!=null) {
+			u2.setLastName("jack"+userInfo.getLastModifiedTime());
+			userInfoRepository.save(u2);
+			System.out.println("模拟事务执行完之后耗时操作........");
+			Thread.sleep(1000*60*2L);
+			System.out.println("耗时操作执行完毕.......");
+		}
 		return userInfoRepository.save(userInfo);
 	}
 }
