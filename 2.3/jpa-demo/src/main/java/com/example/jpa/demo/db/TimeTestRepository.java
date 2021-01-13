6c954d5b98440211a07d26ead3e6a094b9e263a2
Jack.zhang <zhenhua.zhang@alo7.com>
2021-01-13 16:31:18 +0800

    feat: add time zone jdbc test

---
+++ 2.3/jpa-demo/src/main/java/com/example/jpa/demo/db/TimeTestRepository.java
@@ -0,0 +1,6 @@
+package com.example.jpa.demo.db;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TimeTestRepository extends CrudRepository<TimeTest,Long> {
+}
--- 2.3/jpa-demo/src/test/java/com/example/jpa/demo/DemoApplicationTests.java
+++ 2.3/jpa-demo/src/test/java/com/example/jpa/demo/DemoApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.jpa.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Date;
+
+//@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Date date = new Date();
+		System.out.println(date.toString());
 	}
 
 }
