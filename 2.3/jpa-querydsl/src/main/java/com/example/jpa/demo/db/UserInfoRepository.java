3142adbb0b893d8f69c287fe590f0c733ba6a119
zhenhua.zhang@alo7.com <zhenhua.zhang@alo7.com>
2021-04-18 08:17:48 +0800

    feat: add query dsl example

--- 2.3/jpa-querydsl/src/main/java/com/example/jpa/demo/db/UserInfoRepository.java
+++ 2.3/jpa-querydsl/src/main/java/com/example/jpa/demo/db/UserInfoRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
+import java.time.Instant;
+import java.util.Iterator;
+
 /**
  * @author jack
  */
@@ -19,5 +22,13 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Long>, Query
 	default void customize(QuerydslBindings bindings, QUserInfo root) {
 		bindings.bind(root.lastName).first((path,value)-> path.contains(value));
 		bindings.bind(root.name).first((path,value)-> path.startsWith(value));
+		//大于某一个年龄
+		bindings.bind(root.ages).first((path,value)-> path.gt(value));
+		bindings.bind(root.createTime).all(((path, values) -> {
+			// createTime范围查询
+			Iterator<Instant> iterator = (Iterator<Instant>) values.iterator();
+			//因此页面传递参数时，第一个element为小值，第二个为大值
+			return java.util.Optional.of(path.between(iterator.next(), iterator.next()));
+		}));
 	};
 }
