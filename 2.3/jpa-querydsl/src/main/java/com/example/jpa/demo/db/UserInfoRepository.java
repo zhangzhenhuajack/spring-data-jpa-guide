fde73680db00cc65e1359e82bef69e8fc46c496f
Jack.zhang <zhenhua.zhang@alo7.com>
2021-04-18 09:46:21 +0800

    feat: tmp

--- 2.3/jpa-querydsl/src/main/java/com/example/jpa/demo/db/UserInfoRepository.java
+++ 2.3/jpa-querydsl/src/main/java/com/example/jpa/demo/db/UserInfoRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
+import java.time.Instant;
+import java.util.Iterator;
+
 /**
  * @author jack
  */
@@ -19,5 +22,9 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Long>, Query
 	default void customize(QuerydslBindings bindings, QUserInfo root) {
 		bindings.bind(root.lastName).first((path,value)-> path.contains(value));
 		bindings.bind(root.name).first((path,value)-> path.startsWith(value));
+		bindings.bind(root.createTime).all(((path, values) -> { //时间范围查询
+			Iterator<Instant> iterator = (Iterator<Instant>) values.iterator();
+			return java.util.Optional.of(path.between(iterator.next(), iterator.next())); //因此页面传递参数时，第一个element为小值，第二个为大值
+		}));
 	};
 }
